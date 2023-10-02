cf6bea5babab8a98ad4997e39ba54d2b5dbee109
Stutimaha <94779689+Stutimaha@users.noreply.github.com>
2023-10-02 10:08:58 +0530

    V5
    improved UI and some bugs

--- Code.js
+++ Code.js
@@ -3,7 +3,7 @@ function doGet() {
   return HtmlService.createHtmlOutputFromFile('dropZone');
 }
 
-// Function to handle the CSV import based on the provided data
+/// Function to handle the CSV import based on the provided data
 function importCSV(data) {
   try {
     // Get the active spreadsheet
@@ -23,11 +23,18 @@ function importCSV(data) {
       return parseInt(col);
     });
 
-    // Apply filters to the array
-    var filteredArray = array.filter(function (row) {
-      return filters.every(function (filter) {
-        var columnIndex = headerIndexes.indexOf(parseInt(filter.column));
-        if (columnIndex !== -1) {
+    // Prepare the data to be inserted
+    var selectedData = array.map(function (row) {
+      return headerIndexes.map(function (index) {
+        return row[index];
+      });
+    });
+
+    // Apply filters to the data
+    if (filters.length > 0) {
+      selectedData = selectedData.filter(function (row) {
+        return filters.every(function (filter) {
+          var columnIndex = filter.column;
           var cellValue = row[columnIndex];
 
           // Perform filtering based on filter type
@@ -35,11 +42,11 @@ function importCSV(data) {
             case 'equals':
               return cellValue === filter.value;
             case 'contains':
-              return cellValue.includes(filter.value);
+              return cellValue.toString().toLowerCase().includes(filter.value.toString().toLowerCase());
             case 'startsWith':
-              return cellValue.startsWith(filter.value);
+              return cellValue.toString().toLowerCase().startsWith(filter.value.toString().toLowerCase());
             case 'endsWith':
-              return cellValue.endsWith(filter.value);
+              return cellValue.toString().toLowerCase().endsWith(filter.value.toString().toLowerCase());
             case 'notEqual':
               return cellValue !== filter.value;
             case 'greaterThan':
@@ -53,17 +60,19 @@ function importCSV(data) {
             default:
               return true; // Default to true if filter type is not recognized
           }
-        }
-        return false; // Return false if the column index is not found
+        });
       });
+    }
+
+    // Extract only selected columns
+    var selectedColumns = headerIndexes.map(function (index) {
+      return headerRow[index];
     });
 
-    // Append the header row to the filtered array
-    filteredArray.unshift(headerRow);
-
-    // Clear existing data on the sheet and set the values of the filtered array
+    // Insert the header row and set the values of the filtered array
     sheet.clear();
-    sheet.getRange(1, 1, filteredArray.length, headerRow.length).setValues(filteredArray);
+    sheet.getRange(1, 1, 1, selectedColumns.length).setValues([selectedColumns]);
+    sheet.getRange(2, 1, selectedData.length, selectedColumns.length).setValues(selectedData);
 
     return 'CSV imported successfully!';
   } catch (error) {
